c0394ad96b7314b2b89adc3038355a8dd0b8ba60
metulburr <metulburr@gmail.com>
2013-10-11 08:33:03 -0400

    added string identifier

--- files.cpp
+++ files.cpp
@@ -6,22 +6,29 @@
 class File{
     public:
         std::vector<std::string> tokens;
+        std::string filename;
+        
+        File(std::string name) : filename(name){
+            
+        }
+
         
         friend std::ostream& operator<<(std::ostream& stdout, const File& obj);
     
-        void write(std::string filename, std::string sep="\n"){
+        void save(std::string sep="\n"){
             std::ofstream f;
-            f.open(filename);
+            f.open(this->filename);
             for (std::vector<int>::size_type i = 0; i != this->tokens.size(); i++) {
                 f << this->tokens[i] << sep;
             }
             f.close();
         }
 
-        void open(std::string filename){
-            std::string s = "";
+        void load(){
+            this->tokens.clear();
+            std::string s;
             std::ifstream f;
-            f.open(filename);
+            f.open(this->filename);
             while(getline(f, s)){
                 this->tokens.push_back(s);
             }
@@ -31,15 +38,18 @@ class File{
 };
 
 std::ostream& operator<<(std::ostream& stdout, const File& obj){
-    std::string start = "[", end = "]", delim = ", ";
+    std::string start = "[", end = "]", delim = "\", \"";
     std::string s;
     if (obj.tokens.size() > 0){
         s += start;
-        for (auto i: obj.tokens){
-            s += i;
-            if (i != obj.tokens.back()){
+         s += "\"";
+        for(auto it = obj.tokens.begin(); it != obj.tokens.end(); ++it){
+            s += *it;
+            if (&*it != &obj.tokens.back()){ //if addresss of iterator is not address of last element
                 s += delim;
             }
+            else
+                s += "\"";
         }
         s += end;
     }
@@ -52,8 +62,13 @@ std::ostream& operator<<(std::ostream& stdout, const File& obj){
 }
 
 int main(){
-    File file;
-    file.open("save.txt");
-    std::cout << file;
+    File file("save.txt");
+    file.load();
+    std::cout << file << std::endl;
+    file.tokens.push_back("t");
+    file.save();
+    file.load();
+    std::cout << file << std::endl;
+
     
 }
