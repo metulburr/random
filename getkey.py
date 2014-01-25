b8d06c4ffe5bd5f787ccfb9637c7c3f19df367f2
metulburr <micahpage911@yahoo.com>
2014-01-25 11:04:14 -0500

    added terminal portable getkey script

---
+++ getkey.py
@@ -0,0 +1,28 @@
+import sys
+
+if sys.platform[:3] == 'win':
+    import msvcrt
+    def getkey():
+        return msvcrt.getch()
+elif sys.platform[:3] == 'lin':
+    import termios, sys, os
+    TERMIOS = termios
+
+    def getkey():
+        fd = sys.stdin.fileno()
+        old = termios.tcgetattr(fd)
+        new = termios.tcgetattr(fd)
+        new[3] = new[3] & ~TERMIOS.ICANON & ~TERMIOS.ECHO
+        new[6][TERMIOS.VMIN] = 1
+        new[6][TERMIOS.VTIME] = 0
+        termios.tcsetattr(fd, TERMIOS.TCSANOW, new)
+        c = None
+        try:
+            c = os.read(fd, 1)
+        finally:
+            termios.tcsetattr(fd, TERMIOS.TCSAFLUSH, old)
+        return c
+
+while True:
+    k = getkey().decode()
+    print(k)
