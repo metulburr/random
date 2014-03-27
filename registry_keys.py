1a6dc699565dc42975c56efe02ee54a6db97594b
metulburr <micahpage911@yahoo.com>
2014-03-27 01:10:45 -0400

    added registry modification script

---
+++ registry_keys.py
@@ -0,0 +1,40 @@
+
+import ctypes
+import subprocess
+import sys
+import os
+
+if os.name == 'nt':
+	if not ctypes.windll.shell32.IsUserAnAdmin():
+		print('\nThis script requires to be ran with admin privileges\n')
+		sys.exit()
+
+if sys.version[0] == '2':
+	input = raw_input
+	import _winreg as wreg
+else:
+	import winreg as wreg
+
+key = wreg.CreateKey(wreg.HKEY_LOCAL_MACHINE, "Software\\TestCompany\\TestProject")
+# Create new subkey
+wreg.SetValue(key, 'NewSubkey', wreg.REG_SZ, 'testsubkey')
+print(wreg.QueryValue(key, 'NewSubKey'))
+# prints 'testsubkey'
+# Create new value
+wreg.SetValueEx(key, 'ValueName', 0, wreg.REG_SZ, 'testvalue')
+print(wreg.QueryValueEx(key,'ValueName'))
+# prints (u'testvalue', 1)
+key.Close()
+
+
+
+key = wreg.OpenKey(wreg.HKEY_LOCAL_MACHINE, "Software\\TestCompany\\TestProject",0,wreg.KEY_ALL_ACCESS)
+wreg.SetValue(key, 'NewSubkey', wreg.REG_SZ, 'subkey_changed')
+print(wreg.QueryValue(key, 'NewSubkey'))
+# prints 'subkey_changed'
+wreg.SetValueEx(key, 'ValueName', 0, wreg.REG_SZ, 'value_changed')
+print(wreg.QueryValueEx(key, 'ValueName'))
+key.Close()
+
+input('Press Enter')
+
