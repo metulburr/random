58cfb03910c3abb77a000d33537370475a2ebd12
metulburr <micahpage911@yahoo.com>
2014-12-23 23:17:08 -0500

    Update setup_bbfreeze.py

--- setup_bbfreeze.py
+++ setup_bbfreeze.py
@@ -9,3 +9,8 @@ f()
 
 #python setup.py
 #creates a dsit directory wtih libs, using root fil pyroller/pyroller.py
+
+#matplotlib.numerix import error fix
+#in file /usr/local/lib/python2.7/dist-packages/bbfreeze-1.1.3-py2.7.egg/bbfreeze/recipes
+#wrap this lline mf.import_hook("matplotlib.numerix.random_array", m)
+#in an try/except ImportError 
